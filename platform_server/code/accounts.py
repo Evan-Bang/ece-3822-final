0aa4197d57a17e203491a7408245f8f4fe88a9fb
silvermor <silver_morales2@hotmail.com>
2026-04-18 13:32:30 -0400

    Started adding structure to the leaderboard portion of the python server

---
+++ platform_server/code/accounts.py
@@ -0,0 +1,36 @@
+"""
+Player accounts and authentication.
+Create account / login: Dictionary with keys as usernames and passwords stored as encrypted hashes
+View player profile: provide information about play times, games played, scores, etc. from the json database, depending on which stats are public public via flags
+"""
+
+from datastructures.array import ArrayList
+from datastructures.hash_table import HashTable
+
+
+class Profile:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class AccountManager:
+    def __init__(self):
+        self.players = ArrayList()
+
+    def create_account(self, username, password):
+        """
+        Create a new account with the given username and password
+         - Check if username is already taken
+         - If not, create a new Player and add to the list of players
+         - If account was created successfully, return True, else False
+        """
+        pass
+
+    def authenticate(self, username, password):
+        """
+        Authenticate a user with the given username and password
+         - Check if username exists and password matches
+         - If authentication is successful return True, else False
+        """
+        pass
+    
